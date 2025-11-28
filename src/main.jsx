58c053aa612bd5b18a8a82e556f40204244782bd
pedrobinelo <pedro.binelo@ufms.br>
2025-11-28 18:47:38 -0400

    fix: Corrigido basename do BrowserRouter para deployment no GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/CaminhosDoBemEstar">
       <Toaster position="top-right" reverseOrder={false} />
       <Router />
     </BrowserRouter>
